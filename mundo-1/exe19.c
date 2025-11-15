ce334f19d3beb84493f34c93ad9fe9eb49c8cb01
ValterRibeiro1994 <valtertert@gmail.com>
2025-11-15 00:06:09 -0300

    Create exe19.c

---
+++ mundo-1/exe19.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+int gerar_indice_aleatorio(int limite) {
+    return rand() % limite;
+}
+
+char* obter_nome(int tamanho){
+    char* nome = malloc(tamanho);
+    if (nome == NULL) { return NULL; }
+
+    printf("Informe o nome do aluno: ");
+    fgets(nome, tamanho, stdin);
+
+    nome[strcspn(nome, "\n")] = '\0';   // remove o \n, se existir
+    return nome;
+}
+
+int main(void){
+    srand(time(NULL));
+
+    int limite_alunos = 4;
+    int limite_nome_aluno = 20;
+
+    char* alunos[limite_alunos];
+
+    // preenche o vetor criado
+    for (int i = 0; i < limite_alunos; i++){
+        alunos[i] = obter_nome(limite_nome_aluno);
+        printf("Aluno registrado: %s\n", alunos[i]);
+    }
+
+    // sortear o aluno aleatoriamente
+    char* aluno_sorteado = alunos[gerar_indice_aleatorio(limite_alunos)];
+    printf("Aluno Sorteado: %s", aluno_sorteado);
+
+    // libera a memória no final
+    for (int i = 0; i < limite_alunos; i++){
+        free(alunos[i]);
+    }
+
+    return 0;
+}
